cfbb51464a59b957592891485c29d7462a7390ef
João Simões <nogravity00@outlook.com>
2018-04-26 23:25:45 +0100

    Missing documentation

--- src/Fracty/Fraction-Operations.cs
+++ src/Fracty/Fraction-Operations.cs
@@ -1,9 +1,18 @@
-﻿namespace Fracty
+﻿using System;
+
+namespace Fracty
 {
     public partial struct Fraction
     {
         #region Addition
 
+        /// <summary>
+        /// Arithmetic addition of fractions.
+        /// </summary>
+        /// <param name="l">The left value</param>
+        /// <param name="r">The right value</param>
+        /// <returns>The arithmetic result</returns>
+        /// <exception cref="OverflowException"></exception>
         public static Fraction operator +(Fraction l, Fraction r) =>
             l.Denominator == r.Denominator
                 ? new Fraction(l.Numerator + r.Numerator, l.Denominator)
@@ -11,15 +20,43 @@
                     l.Numerator * r.Denominator + r.Numerator * l.Denominator,
                     l.Denominator * r.Denominator);
 
+        /// <summary>
+        /// Arithmetic addition of fractions with an <see cref="int"/>.
+        /// </summary>
+        /// <param name="l">The left value</param>
+        /// <param name="r">The right value</param>
+        /// <returns>The arithmetic result</returns>
+        /// <exception cref="OverflowException"></exception>
         public static Fraction operator +(int l, Fraction r) =>
             new Fraction(l * r.Denominator + r.Numerator, r.Denominator);
 
+        /// <summary>
+        /// Arithmetic addition of fractions with a <see cref="short"/>.
+        /// </summary>
+        /// <param name="l">The left value</param>
+        /// <param name="r">The right value</param>
+        /// <returns>The arithmetic result</returns>
+        /// <exception cref="OverflowException"></exception>
         public static Fraction operator +(short l, Fraction r) =>
             new Fraction(l * r.Denominator + r.Numerator, r.Denominator);
 
+        /// <summary>
+        /// Arithmetic addition of fractions with an <see cref="int"/>.
+        /// </summary>
+        /// <param name="l">The left value</param>
+        /// <param name="r">The right value</param>
+        /// <returns>The arithmetic result</returns>
+        /// <exception cref="OverflowException"></exception>
         public static Fraction operator +(Fraction l, int r) =>
             new Fraction(l.Numerator + r * l.Denominator, l.Denominator);
 
+        /// <summary>
+        /// Arithmetic addition of fractions with a <see cref="short"/>.
+        /// </summary>
+        /// <param name="l">The left value</param>
+        /// <param name="r">The right value</param>
+        /// <returns>The arithmetic result</returns>
+        /// <exception cref="OverflowException"></exception>
         public static Fraction operator +(Fraction l, short r) =>
             new Fraction(l.Numerator + r * l.Denominator, l.Denominator);
 
@@ -27,6 +64,13 @@
 
         #region Subtraction
 
+        /// <summary>
+        /// Arithmetic subtraction of fractions.
+        /// </summary>
+        /// <param name="l">The left value</param>
+        /// <param name="r">The right value</param>
+        /// <returns>The arithmetic result</returns>
+        /// <exception cref="OverflowException"></exception>
         public static Fraction operator -(Fraction l, Fraction r) =>
             l.Denominator == r.Denominator
                 ? new Fraction(l.Numerator - r.Numerator, l.Denominator)
@@ -34,15 +78,43 @@
                     l.Numerator * r.Denominator - r.Numerator * l.Denominator,
                     l.Denominator * r.Denominator);
 
+        /// <summary>
+        /// Arithmetic subtraction of fractions with an <see cref="int"/>.
+        /// </summary>
+        /// <param name="l">The left value</param>
+        /// <param name="r">The right value</param>
+        /// <returns>The arithmetic result</returns>
+        /// <exception cref="OverflowException"></exception>
         public static Fraction operator -(int l, Fraction r) =>
             new Fraction(l * r.Denominator - r.Numerator, r.Denominator);
 
+        /// <summary>
+        /// Arithmetic subtraction of fractions with a <see cref="short"/>.
+        /// </summary>
+        /// <param name="l">The left value</param>
+        /// <param name="r">The right value</param>
+        /// <returns>The arithmetic result</returns>
+        /// <exception cref="OverflowException"></exception>
         public static Fraction operator -(short l, Fraction r) =>
             new Fraction(l * r.Denominator - r.Numerator, r.Denominator);
 
+        /// <summary>
+        /// Arithmetic subtraction of fractions with an <see cref="int"/>.
+        /// </summary>
+        /// <param name="l">The left value</param>
+        /// <param name="r">The right value</param>
+        /// <returns>The arithmetic result</returns>
+        /// <exception cref="OverflowException"></exception>
         public static Fraction operator -(Fraction l, int r) =>
             new Fraction(l.Numerator - r * l.Denominator, l.Denominator);
 
+        /// <summary>
+        /// Arithmetic subtraction of fractions with a <see cref="short"/>.
+        /// </summary>
+        /// <param name="l">The left value</param>
+        /// <param name="r">The right value</param>
+        /// <returns>The arithmetic result</returns>
+        /// <exception cref="OverflowException"></exception>
         public static Fraction operator -(Fraction l, short r) =>
             new Fraction(l.Numerator - r * l.Denominator, l.Denominator);
 
@@ -50,18 +122,53 @@
 
         #region Multiplication
 
+        /// <summary>
+        /// Arithmetic multiplication of fractions.
+        /// </summary>
+        /// <param name="l">The left value</param>
+        /// <param name="r">The right value</param>
+        /// <returns>The arithmetic result</returns>
+        /// <exception cref="OverflowException"></exception>
         public static Fraction operator *(Fraction l, Fraction r) =>
             new Fraction(l.Numerator * r.Numerator, l.Denominator * r.Denominator);
 
+        /// <summary>
+        /// Arithmetic multiplication of fractions with an <see cref="int"/>.
+        /// </summary>
+        /// <param name="l">The left value</param>
+        /// <param name="r">The right value</param>
+        /// <returns>The arithmetic result</returns>
+        /// <exception cref="OverflowException"></exception>
         public static Fraction operator *(int l, Fraction r) =>
             new Fraction(l * r.Numerator, r.Denominator);
 
+        /// <summary>
+        /// Arithmetic multiplication of fractions with a <see cref="short"/>.
+        /// </summary>
+        /// <param name="l">The left value</param>
+        /// <param name="r">The right value</param>
+        /// <returns>The arithmetic result</returns>
+        /// <exception cref="OverflowException"></exception>
         public static Fraction operator *(short l, Fraction r) =>
             new Fraction(l * r.Numerator, r.Denominator);
 
+        /// <summary>
+        /// Arithmetic multiplication of fractions with an <see cref="int"/>.
+        /// </summary>
+        /// <param name="l">The left value</param>
+        /// <param name="r">The right value</param>
+        /// <returns>The arithmetic result</returns>
+        /// <exception cref="OverflowException"></exception>
         public static Fraction operator *(Fraction l, int r) =>
             new Fraction(l.Numerator * r, l.Denominator);
 
+        /// <summary>
+        /// Arithmetic multiplication of fractions with a <see cref="short"/>.
+        /// </summary>
+        /// <param name="l">The left value</param>
+        /// <param name="r">The right value</param>
+        /// <returns>The arithmetic result</returns>
+        /// <exception cref="OverflowException"></exception>
         public static Fraction operator *(Fraction l, short r) =>
             new Fraction(l.Numerator * r, l.Denominator);
 
@@ -69,18 +176,53 @@
 
         #region Division
 
+        /// <summary>
+        /// Arithmetic addition of fractions.
+        /// </summary>
+        /// <param name="l">The left value</param>
+        /// <param name="r">The right value</param>
+        /// <returns>The arithmetic result</returns>
+        /// <exception cref="OverflowException"></exception>
         public static Fraction operator /(Fraction l, Fraction r) =>
             l * r.Reciprocal();
 
+        /// <summary>
+        /// Arithmetic division of fractions with an <see cref="int"/>.
+        /// </summary>
+        /// <param name="l">The left value</param>
+        /// <param name="r">The right value</param>
+        /// <returns>The arithmetic result</returns>
+        /// <exception cref="OverflowException"></exception>
         public static Fraction operator /(int l, Fraction r) =>
             new Fraction(r.Numerator, l * r.Denominator);
 
+        /// <summary>
+        /// Arithmetic division of fractions with a <see cref="short"/>.
+        /// </summary>
+        /// <param name="l">The left value</param>
+        /// <param name="r">The right value</param>
+        /// <returns>The arithmetic result</returns>
+        /// <exception cref="OverflowException"></exception>
         public static Fraction operator /(short l, Fraction r) =>
             new Fraction(r.Numerator, l * r.Denominator);
 
+        /// <summary>
+        /// Arithmetic division of fractions with an <see cref="int"/>.
+        /// </summary>
+        /// <param name="l">The left value</param>
+        /// <param name="r">The right value</param>
+        /// <returns>The arithmetic result</returns>
+        /// <exception cref="OverflowException"></exception>
         public static Fraction operator /(Fraction l, int r) =>
             new Fraction(l.Numerator, l.Denominator * r);
 
+        /// <summary>
+        /// Arithmetic division of fractions with a <see cref="short"/>.
+        /// </summary>
+        /// <param name="l">The left value</param>
+        /// <param name="r">The right value</param>
+        /// <returns>The arithmetic result</returns>
+        /// <exception cref="OverflowException"></exception>
         public static Fraction operator /(Fraction l, short r) =>
             new Fraction(l.Numerator, l.Denominator * r);
 
--- src/Fracty/Fraction.cs
+++ src/Fracty/Fraction.cs
@@ -2,12 +2,35 @@
 
 namespace Fracty
 {
+    /// <summary>
+    /// Fraction structure using <see cref="int"/> values as the
+    /// numerator and denominator.
+    /// </summary>
     public partial struct Fraction
     {
+        /// <summary>
+        /// Represents the zero value (0/1). This field is constant.
+        /// </summary>
         public static readonly Fraction Zero = new Fraction(0, 1);
+
+        /// <summary>
+        /// Represents the largest possible value of a fraction (<see cref="int.MaxValue"/>/1). This field is constant.
+        /// </summary>
+        public static readonly Fraction MaxValue = new Fraction(int.MaxValue, 1);
+
+        /// <summary>
+        /// Represents the smallest possible value of a fraction (<see cref="int.MinValue"/>/1). This field is constant.
+        /// </summary>
+        public static readonly Fraction MinValue = new Fraction(int.MinValue, 1);
 
         private readonly int _denominator;
 
+        /// <summary>
+        /// Creates a new fraction
+        /// </summary>
+        /// <param name="numerator">The fraction numerator</param>
+        /// <param name="denominator">The fraction denominator</param>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
         public Fraction(int numerator, int denominator)
         {
             if (denominator == 0)
@@ -17,12 +40,23 @@ namespace Fracty
             _denominator = denominator;
         }
 
+        /// <summary>
+        /// The fraction numerator
+        /// </summary>
         public int Numerator { get; }
 
+        /// <summary>
+        /// The fraction denominator
+        /// </summary>
         public int Denominator => _denominator == 0 ? 1 : _denominator;
 
+        /// <summary>
+        /// Returns a reciprocal representation of this fraction
+        /// </summary>
+        /// <returns>The reciprocal fraction</returns>
         public Fraction Reciprocal() => new Fraction(Denominator, Numerator);
 
+        /// <inheritdoc />
         public override string ToString() => string.Concat(Numerator, "/", Denominator);
     }
 }
