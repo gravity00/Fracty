a77dcbd6fd10ce60f6d7f4ad301a3c68f6cf4750
João Simões <nogravity00@outlook.com>
2018-04-26 23:27:54 +0100

    Missing license

--- src/Fracty/Fraction-Operations.cs
+++ src/Fracty/Fraction-Operations.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿// Copyright (c) 2018 João Simões.
+// Licensed under the MIT License. See License in the project root for license information.
+
+using System;
 
 namespace Fracty
 {
