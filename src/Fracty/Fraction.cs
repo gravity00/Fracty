5c779e1119148e882bdac908f883053434857bfe
João Simões <nogravity00@outlook.com>
2018-04-26 22:58:41 +0100

    Fraction struct

---
+++ src/Fracty/Fraction.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Fracty
+{
+    public partial struct Fraction
+    {
+        public static readonly Fraction Zero = new Fraction(0, 1);
+
+        private readonly int _denominator;
+
+        public Fraction(int numerator, int denominator)
+        {
+            if (denominator == 0)
+                throw new ArgumentOutOfRangeException(nameof(denominator));
+
+            Numerator = numerator;
+            _denominator = denominator;
+        }
+
+        public int Numerator { get; }
+
+        public int Denominator => _denominator == 0 ? 1 : _denominator;
+
+        public Fraction Reciprocal() => new Fraction(Denominator, Numerator);
+
+        public override string ToString() => string.Concat(Numerator, "/", Denominator);
+    }
+}
