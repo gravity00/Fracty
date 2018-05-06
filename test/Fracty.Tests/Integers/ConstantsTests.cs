6b0a005918a243d6ea5407e80e723cf7d4207bca
João Simões <nogravity00@outlook.com>
2018-05-06 20:36:49 +0100

    Tests for constants

--- test/Fracty.Tests/Integers/ConstantsTests.cs
+++ test/Fracty.Tests/Integers/ConstantsTests.cs
@@ -1,10 +1,34 @@
-﻿namespace Fracty.Tests.Integers
+﻿using Xunit;
+
+namespace Fracty.Tests.Integers
 {
     public class ConstantsTests
     {
-        public void GivenZeroConstantWhenUsingZeroValueThenItMustRepresentZero()
+        [Fact]
+        public void GivenAvailableConstantsWhenUsingZeroValueThenItMustRepresentZero()
         {
+            var fraction = Fraction.ZeroValue;
 
+            Assert.Equal(0, fraction.Numerator);
+            Assert.Equal(1, fraction.Denominator);
+        }
+
+        [Fact]
+        public void GivenAvailableConstantsWhenUsingMaxValueThenItMustInt32MaxValue()
+        {
+            var fraction = Fraction.MaxValue;
+
+            Assert.Equal(int.MaxValue, fraction.Numerator);
+            Assert.Equal(1, fraction.Denominator);
+        }
+
+        [Fact]
+        public void GivenAvailableConstantsWhenUsingMinValueThenItMustInt32MinValue()
+        {
+            var fraction = Fraction.MinValue;
+
+            Assert.Equal(int.MinValue, fraction.Numerator);
+            Assert.Equal(1, fraction.Denominator);
         }
     }
 }
