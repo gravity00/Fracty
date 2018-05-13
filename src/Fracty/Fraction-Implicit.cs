7455a72aec8b9d41b798a0c2bc14e0fe43e5321e
João Simões <nogravity00@outlook.com>
2018-05-13 18:08:00 +0100

    Implicit convertions

---
+++ src/Fracty/Fraction-Implicit.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) 2018 João Simões.
+// Licensed under the MIT License. See License in the project root for license information.
+
+namespace Fracty
+{
+    public partial struct Fraction
+    {
+        /// <summary>
+        /// Implicit convertion of <see cref="int"/> into <see cref="Fraction"/>.
+        /// </summary>
+        /// <param name="value">The value to convert</param>
+        public static implicit operator Fraction(int value) => new Fraction(value, 1);
+
+        /// <summary>
+        /// Implicit convertion of <see cref="short"/> into <see cref="Fraction"/>.
+        /// </summary>
+        /// <param name="value">The value to convert</param>
+        public static implicit operator Fraction(short value) => new Fraction(value, 1);
+    }
+}
